6e0f911b722591b3461fb89226d16c81a47b1fb3
Changho Choi <cch.choi@gmail.com>
2016-07-29 04:19:55 -0400

    finish drawer and actionbar

---
+++ app/src/main/java/com/conestogac/musicplayer/model/PlayList.java
@@ -0,0 +1,17 @@
+package com.conestogac.musicplayer.model;
+
+/**
+ * Created by infomat on 16-07-28.
+ */
+public class PlayList {
+    private String name;
+    public PlayList() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/conestogac/musicplayer/ui/AlbumViewHolder.java
@@ -0,0 +1,11 @@
+package com.conestogac.musicplayer.ui;
+
+/**
+ * Created by infomat on 16-07-28.
+ */
+public class AlbumViewHolder {
+
+    public void AlbumViewHolder() {
+
+    }
+}
