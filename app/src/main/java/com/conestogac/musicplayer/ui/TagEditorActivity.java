77356dc2d19f46b5b3ff22f1a333c77bc0449091
nmcollins <NMcollins-cc@conestogac.on.ca>
2016-08-09 23:42:43 -0400

    Fix Genre Updating
    Genre editing basically works now.

--- app/src/main/java/com/conestogac/musicplayer/ui/TagEditorActivity.java
+++ app/src/main/java/com/conestogac/musicplayer/ui/TagEditorActivity.java
@@ -12,8 +12,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.conestogac.musicplayer.R;
 import com.conestogac.musicplayer.model.Genre;
@@ -23,10 +26,14 @@ import com.conestogac.musicplayer.util.MusicHelper;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class TagEditorActivity extends AppCompatActivity {
 
-
+    int originalGenreId = -1;
+    int genreId = -1;
+    boolean isFirstTime = true;
+    String genreFound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,10 +47,15 @@ public class TagEditorActivity extends AppCompatActivity {
         EditText songYear = (EditText) findViewById(R.id.year);
         EditText songTrack = (EditText) findViewById(R.id.track);
         EditText songComposer = (EditText) findViewById(R.id.composer);
-        EditText genre = (EditText) findViewById(R.id.genre);
+        final EditText genre = (EditText) findViewById(R.id.genre);
+
+        Spinner spinner;
+        ArrayAdapter<CharSequence> adapter;
+        int genreFoundPosition = 0;
 
         Bundle b = getIntent().getExtras();
         final Song currentSong = b.getParcelable("id");
+        final ArrayList<Genre> genreList = MusicHelper.getGenreList(this.getBaseContext());
 
 
         artistName.setText(currentSong.getArtist());
@@ -54,18 +66,18 @@ public class TagEditorActivity extends AppCompatActivity {
         String selection = MediaStore.Audio.AudioColumns._ID + " = ?";
         String arg[] = {String.valueOf(currentSong.getID())};
         String[] projection = new String[]{MediaStore.Audio.Media._ID, MediaStore.Audio.Media.TITLE,
-                                           MediaStore.Audio.Media.ARTIST, MediaStore.Audio.Media.ALBUM_ID,
-                                           MediaStore.Audio.Media.ALBUM, MediaStore.Audio.Media.YEAR,
-                                           MediaStore.Audio.Media.COMPOSER, MediaStore.Audio.Media.TRACK
+                MediaStore.Audio.Media.ARTIST, MediaStore.Audio.Media.ALBUM_ID,
+                MediaStore.Audio.Media.ALBUM, MediaStore.Audio.Media.YEAR,
+                MediaStore.Audio.Media.COMPOSER, MediaStore.Audio.Media.TRACK
 
 
-                                             };
+        };
         String sortOrder = MediaStore.Audio.Media.TITLE + " ASC";
 
         Cursor cursor = getContentResolver().query(URI,projection ,selection, arg, sortOrder);
 
 
-         if (cursor != null && cursor.moveToFirst()) {
+        if (cursor != null && cursor.moveToFirst()) {
             albumTitle.setText(cursor.getString(4));
             songYear.setText(cursor.getString(5));
             songTrack.setText(cursor.getString(7));
@@ -80,18 +92,63 @@ public class TagEditorActivity extends AppCompatActivity {
             });
         }
 
-        ArrayList<Genre> genreList = MusicHelper.getGenreList(this.getBaseContext());
+        final String[] genreNames = new String[genreList.size()];
+
+        List<String> list = new ArrayList<String>();
+
 
         for (int j=0;j<genreList.size();j++) {
             ArrayList<Song> SongsByGenre =  MusicHelper.getSongListByGenre(this.getBaseContext(), genreList.get(j).getID());
+            genreNames[j] = genreList.get(j).getGenre();
+            list.add(genreList.get(j).getGenre());
             for (int k=0; k < SongsByGenre.size(); k++) {
                 if (SongsByGenre.get(k).getID() == currentSong.getID()) {
-                    String genreFound = genreList.get(j).getGenre();
+                    genreFound = genreList.get(j).getGenre();
                     genre.setText(genreFound);
+
+                    originalGenreId = genreList.get(j).getID();
 
                 }
             }
         }
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list); //selected item will look like a spinner set from XML
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerArrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1,
+                                       int position, long id) {
+
+                genreId = genreList.get(position).getID();
+
+                if (isFirstTime) {
+                    genre.setText(genreFound);
+                    isFirstTime = false;
+
+                }
+                else {
+                    genre.setText(genreList.get(position).getGenre());
+                }
+
+                //call calulcate method
+                //calculatePrice();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        });
+
+
+
+
+
+
+
+
     }
 
     public void updateData(Song currentSong) {
@@ -116,8 +173,28 @@ public class TagEditorActivity extends AppCompatActivity {
 
         String selection = MediaStore.Audio.AudioColumns._ID + " = ?";
         String arg[] = {String.valueOf(currentSong.getID())};
-
-
         getContentResolver().update(URI, values, selection, arg);
+
+
+        getContentResolver().delete(MediaStore.Audio.Genres.Members.getContentUri(
+                "external", originalGenreId), MediaStore.Audio.Genres.Members.AUDIO_ID + " = " + currentSong.getID(), null);
+
+        //  getContentResolver().insert(MediaStore.Audio.Genres.Members.getContentUri(
+        //        "external", genreId), MediaStore.Audio.Genres.Members.AUDIO_ID + " = " + currentSong.getID(), null);
+
+        ContentValues values3 = new ContentValues();
+        values3.put(MediaStore.Audio.Genres.Members.AUDIO_ID, currentSong.getID());
+        getContentResolver().insert(MediaStore.Audio.Genres.Members.getContentUri("external", genreId),values3 );
+
+
+
+        // ContentValues values2 = new ContentValues();
+        //   String selection2 = MediaStore.Audio.AudioColumns._ID + " = ?";
+        //   String arg2[] = {String.valueOf(currentSong.getID())};
+        //   Uri musicUri = MediaStore.Audio.Genres.Members.getContentUri("external", genreId);
+        //  values2.put(MediaStore.Audio.Genres.Members.GENRE_ID, genreId);
+        //  getContentResolver().update(musicUri, values2, selection2, arg2);
+
+
     }
 }
